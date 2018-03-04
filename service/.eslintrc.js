b342c2619c2585dd4562a3def7abb9d82b7aa7c4
szopqa <szopqa96@gmail.com>
2018-03-04 13:28:35 +0100

    Added and configured linter with airbnb style configuration

---
+++ service/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "global-require": "off",
+        "import/no-dynamic-require": "off",
+        "no-multiple-empty-lines": ["error", { "max": 1 }],
+        "no-tabs": "off"
+    }
+};
